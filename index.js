3ef2cf2f3c21c0e9c7d549916cc2f6eb0dc9cbb3
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2022-09-23 12:35:59 -0300

    routing

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express(); // Create an express app
 // Define a port to run the server on
 const port = 3000;
 
+// routing
+app.get('/', (req, res) => {
+    res.send('Express!')
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
