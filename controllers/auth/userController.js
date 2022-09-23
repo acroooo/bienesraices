e0ed9120452baff2959462eccfab0f6edc90f0e3
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2022-09-23 18:52:48 -0300

    signupFormlayout

--- controllers/auth/userController.js
+++ controllers/auth/userController.js
@@ -1,12 +1,14 @@
 const loginForm = (req, res) => {
     res.render('./auth/login', {
         state: true,
+        title: 'Iniciar sesión',
     });
 };
 
 const signupForm = (req, res) => {
     res.render('./auth/signup', {
         state: true,
+        title: 'Crear cuenta'
     });
 };
 
