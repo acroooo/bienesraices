3c4083e781addaf166c009ec1c3bf6ad47b39273
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2022-09-27 23:36:00 -0300

    recover-password-view

--- routes/auth/userRoutes.js
+++ routes/auth/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { loginForm, signupForm } from '../../controllers/auth/userController.js';
+import { loginForm, signupForm, recoverPasswordForm } from '../../controllers/auth/userController.js';
 const router = express.Router();
 
 router.get('/login', loginForm);
 router.get('/signup', signupForm);
+router.get('/recover-password', recoverPasswordForm);
 
 export default router;
