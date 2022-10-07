3be0693bc4d0f02b76202c1632b81818261af961
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2022-10-07 03:07:31 -0300

    nodemailer_mailtrap

---
+++ helpers/email.js
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer';
+
+const emailSignup = async (data) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+
+export {
+    emailSignup,
+}
